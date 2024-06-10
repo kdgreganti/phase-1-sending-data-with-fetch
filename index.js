c0e626b3a9c1d391ca841e80687d14aacc3baf09
Kyle Greganti <kyle.greganti@yahoo.com>
2024-06-10 08:44:54 -0500

    meets reqs

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData() {
+    let name = 'Steve';
+    let email = 'steve@steve.com'
+
+return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({ name, email })
+})
+.then(response => response.json())
+.then(data => {
+    let id = data.id;
+    let idElement = document.createElement('p');
+    idElement.textContent = `New ID: ${id}`;
+    document.body.appendChild(idElement);
+})
+.catch(error => {
+    console.error('Error:', error);
+    let errorElement = document.createElement('p');
+    errorElement.textContent = `Error: ${error.message}`;
+    document.body.appendChild(errorElement);
+});
+}
+
